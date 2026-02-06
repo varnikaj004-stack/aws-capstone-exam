e5f5e6255f8c828d895b81a6e946680ebda5f9ac
varnikaj004-stack <varnikaj004@gmail.com>
2026-02-06 10:22:49 +0000

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b"]
+}
