b425ddfaf203e167a0ee93fe18d3356202ffc35e
varnikaj004-stack <varnikaj004@gmail.com>
2026-02-06 10:14:24 +0000

    Create index.php

---
+++ app/v2/index.php
@@ -0,0 +1,7 @@
+<?php
+$ip = $_SERVER['SERVER_ADDR'];
+echo "<body style='background-color:lightblue'>";
+echo "<h1>Welcome to StreamLine - v2 [New Feature]</h1>";
+echo "<p>Server IP: $ip</p>";
+echo "</body>";
+?>
